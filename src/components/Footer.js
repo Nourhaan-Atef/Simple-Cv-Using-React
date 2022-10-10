c62e47286a12503328a963abc4fb81a941a7430d
unknown <anour2168@gmail.com>
2022-10-10 19:08:31 +0200

    Update Social Media Links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,10 +10,18 @@ class Footer extends Component {
           <p>Find Me One Social Media</p>
           </section>
           <section className='middle'>
+            <a href='https://www.facebook.com/nony.atef.77' target='_blank' rel="noreferrer noopener">
             <FaFacebook className='icon'/>
+            </a>
+            <a href='https://twitter.com/AtefNoury?s=08' target='_blank' rel="noreferrer noopener">
             <FaTwitter className='icon'/>
+            </a>
+            <a href='https://github.com/Nourhaan-Atef' target='_blank' rel="noreferrer noopener">
             <FaGithub className='icon'/>
+            </a>
+            <a href='https://www.linkedin.com/in/nourhaan-atef/' target='_blank' rel="noreferrer noopener">
             <FaLinkedin className='icon'/>
+            </a>
           </section>
           <section className='bottom'>
           <p>Designed By <span>Nourhan Atef</span></p>
